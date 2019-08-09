0d8d73e748e833bcae4d057f97c3acc6df9476e6
TahaMagdy <t.tahamagdyt@outlook.com>
2019-08-09 19:25:41 +0200

    refactoring: renaming pipeSprite to pipesArray

--- Pipe.cpp
+++ Pipe.cpp
@@ -13,7 +13,7 @@ namespace game {
         float y = _data->window.getSize().y - sprite.getGlobalBounds().height;
         sprite.setPosition(x, y);
         
-        pipeSprite.push_back(sprite);
+        pipesArray.push_back(sprite);
         
     } ///
     
@@ -23,7 +23,7 @@ namespace game {
         sf::Sprite sprite(_data->assets.getTexture("Pipe Down"));
         float x = _data->window.getSize().x;
         sprite.setPosition(x, 0.0f);
-        pipeSprite.push_back(sprite);
+        pipesArray.push_back(sprite);
         
     } ///
     
@@ -36,23 +36,24 @@ namespace game {
         sprite.setPosition(x, y);
         sprite.setColor(sf::Color(0, 0, 0, 0));
         
-        pipeSprite.push_back(sprite);
+        pipesArray.push_back(sprite);
         
     } ///
     
     void Pipe::movePipes(float dt)
     {
-        for (unsigned short int i = 0; i < pipeSprite.size(); i++)
+        for (unsigned short int i = 0; i < pipesArray.size(); i++)
         {
             // delete the pipe once it goes of the left bound.
-            if (pipeSprite.at(i).getPosition().x < 0 - pipeSprite.at(i).getGlobalBounds().width)
-                pipeSprite.erase(pipeSprite.begin() + i);
+            if (pipesArray.at(i).getPosition().x < 0 - pipesArray.at(i).getGlobalBounds().width)
+                pipesArray.erase(pipesArray.begin() + i);
             else {
                 float movement = CONST::pipe_speed * dt;
-                pipeSprite.at(i).move(-movement, 0);
+                pipesArray.at(i).move(-movement, 0);
             }
             
-            std::cout << pipeSprite.size() << std::endl;
+            // watching the size of the pipe array.
+            // std::cout << pipeSprite.size() << std::endl;
 
         }
 
@@ -64,8 +65,8 @@ namespace game {
     void
     Pipe::drawPipes()
     {
-        for (unsigned short int i = 0; i < pipeSprite.size(); i++)
-            _data->window.draw(pipeSprite.at(i));
+        for (unsigned short int i = 0; i < pipesArray.size(); i++)
+            _data->window.draw(pipesArray.at(i));
     } ///
     
     
