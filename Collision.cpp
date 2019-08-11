a5371db6d8b66514c4b487cb3eabed2c79249fe0
TahaMagdy <t.tahamagdyt@outlook.com>
2019-08-11 21:26:03 +0200

    Bug Fix: scaling rect in collision system

--- Collision.cpp
+++ Collision.cpp
@@ -15,8 +15,8 @@ namespace game {
                                          sf::Sprite s2,
                                          float scale2)
     {
-        s1.setScale(scale1,  scale2);
-        s2.setScale(scale1,  scale2);
+        s1.setScale(scale1,  scale1);
+        s2.setScale(scale2,  scale2);
         
         sf::Rect<float> rect1 = s1.getGlobalBounds();
         sf::Rect<float> rect2 = s2.getGlobalBounds();
